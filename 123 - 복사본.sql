fbebd98d6c8f425f284f0595cbcb8bb462c076a3
LeeHyeongHak <90162385+LeeHyeongHak@users.noreply.github.com>
2021-12-28 09:56:27 +0900

    Delete 123 - 복사본.sql

--- 123 - 복사본.sql
+++
@@ -1,6 +0,0 @@
---데이터 초기화
-drop table member cascade constraints;
-drop table category1 cascade constraints;
-drop sequence seq_category1;
-drop table category2 cascade
-create sequence seq_category1;
